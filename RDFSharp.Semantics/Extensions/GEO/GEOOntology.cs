ab2982c55fc290cc94fcd7107c0ebae0b4468f58
Marco De Salvo <floating_point_9@msn.com>
2023-02-04 12:06:58 +0100

    WIP GeoSPARQL POC

---
+++ RDFSharp.Semantics/Extensions/GEO/GEOOntology.cs
@@ -0,0 +1,43 @@
+﻿/*
+   Copyright 2012-2022 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using RDFSharp.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RDFSharp.Semantics.Extensions.GEO
+{
+    /// <summary>
+    /// GEOOntology represents an OWL ontology specialized in describing relations between GeoSPARQL entities
+    /// </summary>
+    public class GEOOntology : RDFResource
+    {
+        #region Properties
+        /// <summary>
+        /// Knowledge describing the GeoSPARQL ontology
+        /// </summary>
+        internal OWLOntology Ontology { get; set; }
+        #endregion
+
+        #region Ctors
+        /// <summary>
+        /// Builds a GeoSPARQL ontology with the given URI (internal T-BOX is initialized with GeoSPARQL ontology)
+        /// </summary>
+        public GEOOntology(string geoOntologyURI) : base(geoOntologyURI)
+            => Ontology = new OWLOntology(geoOntologyURI) { Model = GEOOntologyLoader.BuildGEOModel() };
+        #endregion
+    }
+}
