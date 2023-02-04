c6b5854823469107aa08976c8cc136a9b665719d
Marco De Salvo <floating_point_9@msn.com>
2023-02-04 14:39:41 +0100

    Begin experiments with Microsoft.Spatial library

--- RDFSharp.Semantics/Extensions/GEO/GEOOntology.cs
+++ RDFSharp.Semantics/Extensions/GEO/GEOOntology.cs
@@ -15,29 +15,116 @@
 */
 
 using RDFSharp.Model;
+using Microsoft.Spatial;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
 
 namespace RDFSharp.Semantics.Extensions.GEO
 {
     /// <summary>
-    /// GEOOntology represents an OWL ontology specialized in describing relations between GeoSPARQL entities
+    /// GEOOntology represents an OWL ontology specialized in describing relations between spatial entities
     /// </summary>
-    public class GEOOntology : RDFResource
+    public class GEOOntology : RDFResource, IEnumerable<RDFResource>
     {
         #region Properties
         /// <summary>
-        /// Knowledge describing the GeoSPARQL ontology
+        /// Count of the spatial objects
+        /// </summary>
+        public long SpatialObjectsCount
+        {
+            get
+            {
+                long count = 0;
+                IEnumerator<RDFResource> pointObjects = PointsEnumerator;
+                while (pointObjects.MoveNext())
+                    count++;
+                return count;
+            }
+        }
+
+        /// <summary>
+        /// Count of the spatial objects of type sf:Point
+        /// </summary>
+        public long PointsCount
+        {
+            get
+            {
+                long count = 0;
+                IEnumerator<RDFResource> spatialObjects = SpatialObjectsEnumerator;
+                while (spatialObjects.MoveNext())
+                    count++;
+                return count;
+            }
+        }
+
+        /// <summary>
+        /// Gets the enumerator on the spatial objects for iteration
+        /// </summary>
+        public IEnumerator<RDFResource> SpatialObjectsEnumerator
+            => Ontology.Data.ABoxGraph[null, RDFVocabulary.RDF.TYPE, RDFVocabulary.GEOSPARQL.SPATIAL_OBJECT, null]
+                            .Select(t => t.Subject)
+                            .OfType<RDFResource>()
+                            .GetEnumerator();
+
+        /// <summary>
+        /// Gets the enumerator on the spatial objects of type sf:Point for iteration
+        /// </summary>
+        public IEnumerator<RDFResource> PointsEnumerator
+            => Ontology.Data.ABoxGraph[null, RDFVocabulary.RDF.TYPE, new RDFResource("http://www.opengis.net/ont/sf#Point"), null]
+                            .Select(t => t.Subject)
+                            .OfType<RDFResource>()
+                            .GetEnumerator();
+
+        /// <summary>
+        /// Knowledge describing the spatial ontology
         /// </summary>
         internal OWLOntology Ontology { get; set; }
         #endregion
 
         #region Ctors
         /// <summary>
-        /// Builds a GeoSPARQL ontology with the given URI (internal T-BOX is initialized with GeoSPARQL ontology)
+        /// Builds a spatial ontology with the given URI (internal T-BOX is initialized with GeoSPARQL ontology)
         /// </summary>
         public GEOOntology(string geoOntologyURI) : base(geoOntologyURI)
             => Ontology = new OWLOntology(geoOntologyURI) { Model = GEOOntologyLoader.BuildGEOModel() };
+        #endregion
+
+        #region Interfaces
+        /// <summary>
+        /// Exposes a typed enumerator on the spatial objects for iteration
+        /// </summary>
+        IEnumerator<RDFResource> IEnumerable<RDFResource>.GetEnumerator()
+            => SpatialObjectsEnumerator;
+
+        /// <summary>
+        /// Exposes an untyped enumerator on the spatial objects for iteration
+        /// </summary>
+        IEnumerator IEnumerable.GetEnumerator()
+            => SpatialObjectsEnumerator;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Declares the given point instance to the spatial ontology (coordinates should be expressed in WGS84)
+        /// </summary>
+        public GEOOntology DeclarePoint(RDFResource pointUri, double latitude, double longitude)
+        {
+            if (pointUri == null)
+                throw new OWLSemanticsException("Cannot declare point instance to the concept scheme because given \"pointUri\" parameter is null");
+
+            //Add knowledge to the A-BOX
+            Ontology.Data.DeclareIndividual(pointUri);
+            Ontology.Data.DeclareIndividualType(pointUri, RDFVocabulary.GEOSPARQL.GEOMETRY);
+            Ontology.Data.DeclareIndividualType(pointUri, new RDFResource("http://www.opengis.net/ont/sf#Point"));
+
+            //Create spatial point
+            GeographyPoint geoPoint = GeographyFactory.Point(latitude, longitude);
+            string wktPoint = WellKnownTextSqlFormatter.Create().Write(geoPoint);
+            Ontology.Data.DeclareDatatypeAssertion(pointUri, RDFVocabulary.GEOSPARQL.AS_WKT, new RDFPlainLiteral($"{wktPoint}^^{RDFVocabulary.GEOSPARQL.WKT_LITERAL}"));
+
+            return this;
+        }
         #endregion
     }
 }
--- RDFSharp.Semantics/Extensions/GEO/GEOOntologyLoader.cs
+++ RDFSharp.Semantics/Extensions/GEO/GEOOntologyLoader.cs
@@ -15,26 +15,24 @@
 */
 
 using RDFSharp.Model;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace RDFSharp.Semantics.Extensions.GEO
 {
     /// <summary>
-    /// GEOOntologyLoader is responsible for loading GeoSPARQL ontologies from remote sources or alternative representations
+    /// GEOOntologyLoader is responsible for loading spatial ontologies from remote sources or alternative representations
     /// </summary>
     internal static class GEOOntologyLoader
     {
         #region Methods
         /// <summary>
-        /// Gets a GeoSPARQL ontology representation of the given graph
+        /// Gets a spatial ontology representation of the given graph
         /// </summary>
         internal static GEOOntology FromRDFGraph(RDFGraph graph, OWLOntologyLoaderOptions loaderOptions)
         {
             if (graph == null)
                 throw new OWLSemanticsException("Cannot get GEO ontology from RDFGraph because given \"graph\" parameter is null");
 
-            //Get OWL ontology with GeoSPARQL extension points
+            //Get OWL ontology with spatial extension points
             OWLOntology ontology = OWLOntologyLoader.FromRDFGraph(graph, loaderOptions,
                classModelExtensionPoint: GEOClassModelExtensionPoint,
                propertyModelExtensionPoint: GEOPropertyModelExtensionPoint,
@@ -46,19 +44,19 @@ namespace RDFSharp.Semantics.Extensions.GEO
 
         #region Utilities
         /// <summary>
-        /// Extends OWL class model loading with support for GeoSPARQL artifacts
+        /// Extends OWL class model loading with support for spatial artifacts
         /// </summary>
         internal static void GEOClassModelExtensionPoint(OWLOntology ontology, RDFGraph graph)
             => ontology.Model.ClassModel = BuildGEOClassModel(ontology.Model.ClassModel);
 
         /// <summary>
-        /// Extends OWL property model loading with support for GeoSPARQL artifacts
+        /// Extends OWL property model loading with support for spatial artifacts
         /// </summary>
         internal static void GEOPropertyModelExtensionPoint(OWLOntology ontology, RDFGraph graph)
             => ontology.Model.PropertyModel = BuildGEOPropertyModel(ontology.Model.PropertyModel);
 
         /// <summary>
-        /// Extends OWL data loading with support for GeoSPARQL artifacts
+        /// Extends OWL data loading with support for spatial artifacts
         /// </summary>
         internal static void GEODataExtensionPoint(OWLOntology ontology, RDFGraph graph)
         { 
@@ -66,18 +64,19 @@ namespace RDFSharp.Semantics.Extensions.GEO
         }
 
         /// <summary>
-        /// Builds a reference GeoSPARQL model
+        /// Builds a reference spatial model
         /// </summary>
         internal static OWLOntologyModel BuildGEOModel()
             => new OWLOntologyModel() { ClassModel = BuildGEOClassModel(), PropertyModel = BuildGEOPropertyModel() };
 
         /// <summary>
-        /// Builds a reference GeoSPARQL class model
+        /// Builds a reference spatial class model
         /// </summary>
         internal static OWLOntologyClassModel BuildGEOClassModel(OWLOntologyClassModel existingClassModel = null)
         {
             OWLOntologyClassModel classModel = existingClassModel ?? new OWLOntologyClassModel();
 
+            //GeoSPARQL
             classModel.DeclareClass(RDFVocabulary.GEOSPARQL.SPATIAL_OBJECT);
             classModel.DeclareClass(RDFVocabulary.GEOSPARQL.GEOMETRY);
             classModel.DeclareClass(RDFVocabulary.GEOSPARQL.FEATURE);
@@ -85,16 +84,63 @@ namespace RDFSharp.Semantics.Extensions.GEO
             classModel.DeclareSubClasses(RDFVocabulary.GEOSPARQL.FEATURE, RDFVocabulary.GEOSPARQL.SPATIAL_OBJECT);
             classModel.DeclareDisjointClasses(RDFVocabulary.GEOSPARQL.GEOMETRY, RDFVocabulary.GEOSPARQL.FEATURE);
 
+            //Simple Features (Geometry)
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#Point"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#Curve"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#Surface"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#Polygon"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#Triangle"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#LineString"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#LinearRing"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#Line"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#GeometryCollection"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#MultiPoint"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#MultiCurve"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#MultiSurface"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#MultiPolygon"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#MultiLineString"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#PolyhedralSurface"));
+            classModel.DeclareClass(new RDFResource("http://www.opengis.net/ont/sf#TIN"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Point"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Curve"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Surface"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Polygon"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Polygon"), new RDFResource("http://www.opengis.net/ont/sf#Surface"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Triangle"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Triangle"), new RDFResource("http://www.opengis.net/ont/sf#Polygon"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#LineString"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#LineString"), new RDFResource("http://www.opengis.net/ont/sf#Curve"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#LinearRing"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#LinearRing"), new RDFResource("http://www.opengis.net/ont/sf#LineString"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Line"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#Line"), new RDFResource("http://www.opengis.net/ont/sf#LineString"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#GeometryCollection"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiPoint"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiPoint"), new RDFResource("http://www.opengis.net/ont/sf#GeometryCollection"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiCurve"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiCurve"), new RDFResource("http://www.opengis.net/ont/sf#GeometryCollection"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiSurface"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiSurface"), new RDFResource("http://www.opengis.net/ont/sf#GeometryCollection"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiPolygon"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiPolygon"), new RDFResource("http://www.opengis.net/ont/sf#MultiSurface"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiLineString"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#MultiLineString"), new RDFResource("http://www.opengis.net/ont/sf#MultiCurve"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#PolyhedralSurface"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#PolyhedralSurface"), new RDFResource("http://www.opengis.net/ont/sf#Surface"));
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#TIN"), RDFVocabulary.GEOSPARQL.GEOMETRY);
+            classModel.DeclareSubClasses(new RDFResource("http://www.opengis.net/ont/sf#TIN"), new RDFResource("http://www.opengis.net/ont/sf#PolyhedralSurface"));
+
             return classModel;
         }
 
         /// <summary>
-        /// Builds a reference GeoSPARQL property model
+        /// Builds a reference spatial property model
         /// </summary>
         internal static OWLOntologyPropertyModel BuildGEOPropertyModel(OWLOntologyPropertyModel existingPropertyModel = null)
         {
             OWLOntologyPropertyModel propertyModel = existingPropertyModel ?? new OWLOntologyPropertyModel();
 
+            //GeoSPARQL
             propertyModel.DeclareObjectProperty(RDFVocabulary.GEOSPARQL.HAS_GEOMETRY, new OWLOntologyObjectPropertyBehavior() { Domain = RDFVocabulary.GEOSPARQL.FEATURE, Range = RDFVocabulary.GEOSPARQL.GEOMETRY });
             propertyModel.DeclareObjectProperty(RDFVocabulary.GEOSPARQL.DEFAULT_GEOMETRY, new OWLOntologyObjectPropertyBehavior() { Domain = RDFVocabulary.GEOSPARQL.FEATURE, Range = RDFVocabulary.GEOSPARQL.GEOMETRY });
             propertyModel.DeclareObjectProperty(RDFVocabulary.GEOSPARQL.SF_CONTAINS, new OWLOntologyObjectPropertyBehavior() { Domain = RDFVocabulary.GEOSPARQL.SPATIAL_OBJECT, Range = RDFVocabulary.GEOSPARQL.SPATIAL_OBJECT });
@@ -133,6 +179,9 @@ namespace RDFSharp.Semantics.Extensions.GEO
             propertyModel.DeclareSubProperties(RDFVocabulary.GEOSPARQL.AS_WKT, RDFVocabulary.GEOSPARQL.HAS_SERIALIZATION);
             propertyModel.DeclareSubProperties(RDFVocabulary.GEOSPARQL.AS_GML, RDFVocabulary.GEOSPARQL.HAS_SERIALIZATION);
             propertyModel.DeclareInverseProperties(RDFVocabulary.GEOSPARQL.EH_COVERS, RDFVocabulary.GEOSPARQL.EH_COVERED_BY);
+
+            //Simple Features (Geometry)
+
 
             return propertyModel;
         }
